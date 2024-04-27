e65cf7cb8259eda0a522ed771b56525120209995
AverageChaos <creepercrunch3@gmail.com>
2024-04-27 17:25:48 -0500

    Fixed compat between SGuid and SAGuid

--- Guids/SerializableAssetGuid.cs
+++ Guids/SerializableAssetGuid.cs
@@ -11,5 +11,14 @@ namespace UnturnedSerializables.Guids
         public SerializableAssetGuid() : base() { }
         public SerializableAssetGuid(string guidString) : base(guidString) { }
         public SerializableAssetGuid(Guid guid) : base(guid) { }
+
+        public static implicit operator Guid(SerializableAssetGuid rValue) => rValue.Guid;
+        public static implicit operator SerializableAssetGuid(Guid rValue) => new SerializableAssetGuid(rValue);
+        public static implicit operator string(SerializableAssetGuid rValue) => rValue.GuidString;
+        public static implicit operator SerializableAssetGuid(string rValue) => new SerializableAssetGuid(rValue);
+
+        public override bool Equals(object obj) => (obj is Guid guid && guid == Guid) || (obj is SerializableGuid sGuid && sGuid.Guid == Guid) || (obj is string guidStr && guidStr == GuidString);
+
+        public override int GetHashCode() => Guid.GetHashCode();
     }
 }
