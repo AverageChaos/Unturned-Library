f61eca77d168653e8f786ac0d76af9e2bc7dc8df
AverageChaos <creepercrunch3@gmail.com>
2024-11-12 03:26:27 -0600

    v1.0.0

---
+++ Serializables/SerializableDictionary.cs
@@ -0,0 +1,57 @@
+﻿using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace ChaosUnturnedLib.Serializables
+{
+    [XmlRoot("Dictionary")]
+    public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, IXmlSerializable where TKey : notnull
+    {
+        public XmlSchema? GetSchema() => null;
+
+        public void ReadXml(XmlReader reader)
+        {
+            if (reader.IsEmptyElement)
+                return;
+
+            var keySerializer = new XmlSerializer(typeof(TKey));
+            var valueSerializer = new XmlSerializer(typeof(TValue));
+
+            reader.ReadStartElement();
+
+            while (reader.IsStartElement("Item"))
+            {
+                reader.ReadStartElement("Item");
+
+                try
+                {
+                    TKey key = (TKey)keySerializer.Deserialize(reader);
+                    TValue value = (TValue)valueSerializer.Deserialize(reader);
+
+                    Add(key, value);
+                }
+                catch (Exception)
+                {
+                }
+
+                reader.ReadEndElement();
+            }
+
+            reader.ReadEndElement();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            var keySerializer = new XmlSerializer(typeof(TKey));
+            var valueSerializer = new XmlSerializer(typeof(TValue));
+
+            foreach (var kvp in this)
+            {
+                writer.WriteStartElement("Item");
+                keySerializer.Serialize(writer, kvp.Key);
+                valueSerializer.Serialize(writer, kvp.Value);
+                writer.WriteEndElement();
+            }
+        }
+    }
+}
---
+++ Serializables/Unturned/AssetGuid.cs
@@ -0,0 +1,31 @@
+﻿using SDG.Unturned;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ChaosUnturnedLib.Serializables.Unturned
+{
+    public struct AssetGuid
+    {
+        [XmlAttribute("AssetName")]
+        public readonly string AssetName
+        { 
+            get => Assets.find(Guid)?.FriendlyName ?? "";
+            set => _ = value;
+        }
+
+        [XmlAttribute("Guid")]
+        public Guid Guid;
+
+        public AssetGuid(string guidString) => Guid = Guid.TryParse(guidString, out Guid guid) ? guid : Guid.Empty;
+        public AssetGuid(Guid guid) => Guid = guid;
+
+        public static implicit operator Guid(AssetGuid rValue) => rValue.Guid;
+        public static implicit operator AssetGuid(Guid rValue) => new(rValue);
+        public static implicit operator string(AssetGuid rValue) => rValue.Guid.ToString();
+        public static implicit operator AssetGuid(string rValue) => new(rValue);
+
+        public override readonly bool Equals(object obj) => obj is Guid guid && guid == Guid || obj is AssetGuid aGuid && aGuid.Guid == Guid || obj is string guidStr && Guid.Parse(guidStr) == Guid;
+
+        public override readonly int GetHashCode() => Guid.GetHashCode();
+    }
+}
