df61a454f82990479fb2125d64e90077fce216ef
AverageChaos <creepercrunch3@gmail.com>
2024-11-13 02:34:26 -0600

    Removed Threaded Attribute

--- Attributes/ThreadedAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace ChaosLib.Attributes
-{
-    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
-    public sealed class ThreadedAttribute : Attribute
-    {
-    }
-}
